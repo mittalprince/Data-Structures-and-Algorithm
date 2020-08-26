ff943a25555d3f4133bc549ddd7b0c3caab79fef
mittalprince <princemittal766@gmail.com>
2020-08-26 14:19:53 +0530

    Leetcode Daily Challenge

---
+++ LeetCode/Fizz-Buzz.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/fizz-buzz/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+vector<string> fizzBuzz(int n) {
+    vector<string>ans;
+
+    for(int i=1; i<=n; i++){
+        string t="";
+        if(i%3==0)t += "Fizz";
+        if(i%5==0)t += "Buzz";
+        if(t.empty())t = to_string(i);
+
+        ans.push_back(t);
+    }
+    return ans;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
