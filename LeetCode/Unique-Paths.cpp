4625fd33ec260ef71a147a395e1a4d8c651b471e
mittalprince <princemittal766@gmail.com>
2020-09-20 16:14:20 +0530

    Leetcode Daily Question

---
+++ LeetCode/Unique-Paths.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/unique-paths/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+int uniquePaths(int m, int n) {
+    vector<vector<int> >dp(m, vector<int>(n, 0));
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 or j==0){
+                dp[i][j]=1;
+            }
+            else{
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
