e73815b53aae493d5df39458793761a95f86d99e
mittalprince <princemittal766@gmail.com>
2020-09-28 21:28:42 +0530

    Leetcode Daily Question

--- LeetCode/Evaluate-Division.cpp
+++ LeetCode/Evaluate-Division.cpp
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/evaluate-division/
 #include<iostream>
 #include<vector>
+#include<unordered_map>
+#include<unordered_set>
 using namespace std;
 typedef long long ll;
 #define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
@@ -12,6 +14,48 @@ typedef long long ll;
 #define INF 1e16
 #define MOD 1000000007
 
+unordered_map<string, vector<pair<string, double> > >adj;
+
+double findValue(string src, string dest, unordered_set<string>visited){
+    if(src==dest)return 1.0;
+
+    visited.insert(src);
+    for(pair<string, double> child: adj[src]){
+        if(visited.count(child.first))continue;
+        double res = findValue(child.first, dest, visited);
+        if(res>0) return child.second*res;
+    }
+
+    return -1.0;
+}
+
+vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    int n=equations.size();
+    for(int i=0; i<n; i++){
+        adj[equations[i][0]].push_back({equations[i][1], values[i]});
+        adj[equations[i][1]].push_back({equations[i][0], 1.0/values[i]});
+    }
+
+    vector<double>ans;
+
+    for(vector<string> q: queries){
+        string src = q[0];
+        string dest = q[1];
+
+        if(adj.count(src)==0 or adj.count(dest)==0){
+            ans.push_back(-1.0);
+        }
+        else{
+           unordered_set<string>visited;
+            double res = findValue(src, dest, visited);
+            ans.push_back(res);
+        }
+    }
+    return ans;
+}
 
 int main(){
 
