f28d9de38d4a2edae619570988df6d3dbe0930f4
mittalprince <princemittal766@gmail.com>
2020-10-28 22:29:36 +0530

    Leetcode Daily Question

---
+++ LeetCode/Summary-Ranges.cpp
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/summary-ranges/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+vector<string> summaryRanges(vector<int>& nums) {
+    const int size_n = nums.size();
+    vector<string> res;
+    if ( 0 == size_n) return res;
+    for (int i = 0; i < size_n;) {
+        int start = i, end = i;
+        while (end + 1 < size_n && nums[end+1] == nums[end] + 1) end++;
+        if (end > start) res.push_back(to_string(nums[start]) + "->" + to_string(nums[end]));
+        else res.push_back(to_string(nums[start]));
+        i = end+1;
+    }
+    return res;
+}
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
