cfc8e56d0c25fa2513ff5e3000f7f02aee702f1b
mittalprince <princemittal766@gmail.com>
2020-11-16 20:53:21 +0530

    Leetcode Daily Question

---
+++ LeetCode/Range-Sum-of-BST.cpp
@@ -0,0 +1,55 @@
+//https://leetcode.com/problems/range-sum-of-bst/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void solve(TreeNode *root, int &ans, int low, int high){
+    if(root==NULL)return;
+    if(root->val>=low and root->val<=high){
+        ans += root->val;
+    }
+
+    solve(root->left, ans, low, high);
+    solve(root->right, ans, low, high);
+}
+
+int rangeSumBST(TreeNode* root, int low, int high) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int ans=0;
+    solve(root, ans, low, high);
+    return ans;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
