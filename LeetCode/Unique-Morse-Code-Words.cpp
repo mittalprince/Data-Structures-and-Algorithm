142a994760160b095a0c1354003d6a64b5e6f796
mittalprince <princemittal766@gmail.com>
2020-11-22 18:47:29 +0530

    Leetcode Daily Question

---
+++ LeetCode/Unique-Morse-Code-Words.cpp
@@ -0,0 +1,45 @@
+//https://leetcode.com/problems/unique-morse-code-words/
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+int uniqueMorseRepresentations(vector<string>& words) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string arr[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+    map<string, int>m;
+    for(string word:words){
+        string t="";
+        for(char i: word){
+            t += arr[i-'a'];
+        }
+        m[t]++;
+    }
+    return m.size();
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
