fc07a32808fe9b1868ab8074dc5269e8951b7f31
mittalprince <princemittal766@gmail.com>
2020-11-25 19:09:48 +0530

    Leetcode Practice Questions

---
+++ LeetCode/Evaluate-Reverse-Polish-Notation.cpp
@@ -0,0 +1,68 @@
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+int evalRPN(vector<string>& tokens) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    stack<int>s;
+    int ans=0;
+
+    for(string i: tokens){
+        if(i=="+"){
+            int a=s.top();s.pop();
+            int b=s.top();s.pop();
+
+            s.push(a+b);
+        }
+        else if(i=="-"){
+            int a=s.top();s.pop();
+            int b=s.top();s.pop();
+
+            s.push(b-a);
+        }
+        else if(i=="*"){
+            int a=s.top();s.pop();
+            int b=s.top();s.pop();
+
+            s.push(a*b);
+        }
+        else if(i=="/"){
+            int a=s.top();s.pop();
+            int b=s.top();s.pop();
+
+            s.push(b/a);
+        }
+        else{
+            s.push(stoi(i));
+        }
+    }
+
+    return s.top();
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
