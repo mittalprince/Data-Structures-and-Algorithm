0fb568456fcf06ae01e314f457e0d105d2009adf
mittalprince <princemittal766@gmail.com>
2020-12-07 21:02:40 +0530

    Throttling in JS

---
+++ Javscript/throttling.js
@@ -0,0 +1,29 @@
+// Throttling in JS
+
+const expensive = ()=>{
+    console.log("Fetching...");
+}
+
+const throttle = function(fn, delay){
+    let flag=true;
+    return function(){
+        let context=this, args=arguments;
+        // calls fn only whenflag is set
+        // once we call fn we set false flag and again set after certain dealy
+        if(flag){
+            fn();
+            flag=false;
+            setTimeout(()=>{
+                flag=true;
+            },delay);
+        }
+    }
+}
+
+const bettweExpensive = throttle(expensive, 500);
+
+/*
+Throttling or sometimes is also called throttle function is a practice used in websites. 
+Throttling is used to call a function after every millisecond or a particular interval of 
+time only the first click is executed immediately.
+*/
