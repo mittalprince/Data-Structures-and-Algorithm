7506f92e4a457608f114a8300da46ce1690af28a
mittalprince <princemittal766@gmail.com>
2020-12-12 13:47:39 +0530

    Leetcode Practice Question

---
+++ LeetCode/Lowest-Common-Ancestor-of-a-Binary-Tree.cpp
@@ -0,0 +1,50 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    if(root==NULL)return root;
+
+    if(root == p)return root;
+    if(root == q)return root;
+
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+    if(left and right)return root;
+    return left?left:right;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
