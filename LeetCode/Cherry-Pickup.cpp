4941833244eb63be560e22b34e7134a86417242c
mittalprince <princemittal766@gmail.com>
2020-12-19 15:57:36 +0530

    Leetcode Practice QUestion

--- LeetCode/Cherry-Pickup.cpp
+++ LeetCode/Cherry-Pickup.cpp
@@ -14,6 +14,24 @@ typedef long long ll;
 #define INF 1e16
 #define MOD 1000000007
 
+/*
+Assupmtion:
+    Go from (0, 0) -> (n-1, n-1) -> (0, 0) can be opt to two men go from (0, 0) -> (n-1, n-1) together, but when they go into
+the same cell, the cur state can only be added 1 (use once)
+Using DP to solve the problem:
+    1.  dp[x1][y1][x2] to represent the largest ans we can get when first guy (marked as A) at(x1, y2) and second guy(marked as B) at (x2, x1 + y1 - x2)
+        * because we can only go right and down, so we have x1 + y1 = x2 + y2
+    2.  Induction: every time we calculate the maximum of :
+        * dp[x1 - 1][y1][x2] : A down, B right
+        * dp[x1][y1 - 1][x2] : A right, B right
+        * dp[x1 - 1][y1][x2 - 1]: A down, B down
+        * dp[x1][y1 - 1][x2 - 1]: A right, B down
+        if the Max of these values is negative, then we don't have a path to this point
+        else we have: dp[x1][y1][x2] = Max + grid[x1 - 1][y1 - 1] + grid[x2 - 1][y2 - 1](if x1 != x2 && y1 != y2) else we
+        only add once.
+
+*/
+        
 int dp[51][51][51];
 int helper(vector<vector<int>>& grid,int r1,int c1,int c2,int n){
     int r2=r1+c1-c2;
