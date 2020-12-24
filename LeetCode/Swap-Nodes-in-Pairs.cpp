fa89fb48c89ec203b09163262aa44ba4ce5e19e1
mittalprince <princemittal766@gmail.com>
2020-12-24 17:59:20 +0530

    Leetcode Daily Question

---
+++ LeetCode/Swap-Nodes-in-Pairs.cpp
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/swap-nodes-in-pairs/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* swapPairs(ListNode* head) {
+    if(head==NULL||head->next==NULL) return head;
+
+    ListNode *t=head->next, *t2=head->next->next;
+    head->next->next=head;
+    head->next = swapPairs(t2);
+    return t;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
