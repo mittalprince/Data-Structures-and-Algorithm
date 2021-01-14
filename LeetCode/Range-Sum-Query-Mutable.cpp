e00d3355bbcf3120e546c4f13ecc96c8403f04ca
mittalprince <princemittal766@gmail.com>
2021-01-14 22:58:54 +0530

    Segment Tree Leetcode

--- LeetCode/Range-Sum-Query-Mutable.cpp
+++ LeetCode/Range-Sum-Query-Mutable.cpp
@@ -76,6 +76,92 @@ public:
     }
 };
 
+// Using Segment Tree
+class NumArray
+{
+    class SegmentTreeNode
+    {
+    public:
+        int start, end, sum;
+        SegmentTreeNode *left, *right;
+        SegmentTreeNode(int a, int b) : start(a), end(b), sum(0), left(NULL), right(NULL) {}
+    };
+
+    SegmentTreeNode *root;
+
+public:
+    NumArray(vector<int> &nums)
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n = nums.size();
+        root = buildTree(nums, 0, n - 1);
+    }
+
+    void update(int i, int val)
+    {
+        modifyTree(i, val, root);
+    }
+
+    int sumRange(int i, int j)
+    {
+        return queryTree(i, j, root);
+    }
+    SegmentTreeNode *buildTree(vector<int> &nums, int start, int end)
+    {
+        if (start > end)
+            return nullptr;
+        SegmentTreeNode *root = new SegmentTreeNode(start, end);
+        if (start == end)
+        {
+            root->sum = nums[start];
+            return root;
+        }
+        int mid = start + (end - start) / 2;
+        root->left = buildTree(nums, start, mid);
+        root->right = buildTree(nums, mid + 1, end);
+        root->sum = root->left->sum + root->right->sum;
+        return root;
+    }
+    int modifyTree(int i, int val, SegmentTreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        int diff;
+        if (root->start == i && root->end == i)
+        {
+            diff = val - root->sum;
+            root->sum = val;
+            return diff;
+        }
+        int mid = (root->start + root->end) / 2;
+        if (i > mid)
+        {
+            diff = modifyTree(i, val, root->right);
+        }
+        else
+        {
+            diff = modifyTree(i, val, root->left);
+        }
+        root->sum = root->sum + diff;
+        return diff;
+    }
+    int queryTree(int i, int j, SegmentTreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        if (root->start == i && root->end == j)
+            return root->sum;
+        int mid = (root->start + root->end) / 2;
+        if (i > mid)
+            return queryTree(i, j, root->right);
+        if (j <= mid)
+            return queryTree(i, j, root->left);
+        return queryTree(i, mid, root->left) + queryTree(mid + 1, j, root->right);
+    }
+};
+
 int main()
 {
 
