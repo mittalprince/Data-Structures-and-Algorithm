79257d0cad2c1617343bf366fc3cce84eabb47f3
mittalprince <princemittal766@gmail.com>
2021-01-17 23:08:10 +0530

    Refactored Code

--- LeetCode/Maximal-Rectangle.cpp
+++ LeetCode/Maximal-Rectangle.cpp
@@ -1,67 +1,98 @@
-#include<iostream>
-#include<vector>
-#include<stack>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
 using namespace std;
 typedef long long ll;
-#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
-#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
-#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ip(arr, n)              \
+    for (int i = 0; i < n; i++) \
+        cin >> arr[i];
+#define op(arr, n)              \
+    for (int i = 0; i < n; i++) \
+        cout << arr[i] << " ";
+#define fstIO                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
 
-int largestRectangleArea(vector<char>& arr) {
-    stack<int>s;
-    int area=0, i=0;
+int findLargestArea(vector<int> &arr)
+{
+    stack<int> s;
+    int area = 0, i = 0;
+    int n = arr.size();
 
-    for(i=0; i<arr.size(); i++){
-        if(!s.empty() && arr[i] <= arr[s.top()]){
-            while(!s.empty() && arr[i] <= arr[s.top()]){
-                int ht=arr[s.top()]-'0';
-                s.pop();   
-                area = max(area, (ht*(s.empty()?i:i-s.top()-1)));
+    for (i = 0; i < n;)
+    {
+        if (!s.empty() and arr[i] <= arr[s.top()])
+        {
+            int ht = arr[s.top()];
+            s.pop();
+            if (s.empty())
+            {
+                area = max(area, ht * i);
+            }
+            else
+            {
+                area = max(area, ht * (i - s.top() - 1));
             }
         }
-        s.push(i);
+        else
+            s.push(i++);
     }
 
-    while(!s.empty()){
-        int ht=arr[s.top()]-'0';
-        s.pop();   
-        area = max(area, (ht*(s.empty()?i:i-s.top()-1)));
+    while (!s.empty())
+    {
+        int ht = arr[s.top()];
+        s.pop();
+        if (s.empty())
+        {
+            area = max(area, ht * i);
+        }
+        else
+        {
+            area = max(area, ht * (i - s.top() - 1));
+        }
     }
     return area;
 }
 
-int maximalRectangle(vector<vector<char> >&arr){
-    int area=0;
-    int n=arr.size();
-    if(n==0) return 0;
-    
-    int m=arr[0].size();
-    
-    int ct=0;
-    for(int j=0; j<m; j++){
-        if(arr[0][j]=='1') ct++;
-        else{
-            area = max(area, ct);
-            ct=0;
-        }
-    }
-    area = max(area, ct);
-    for(int i=1; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(arr[i][j] == '1'){
-                arr[i][j] = ((arr[i][j]-'0') +(arr[i-1][j]-'0'))+'0';
+int maximalRectangle(vector<vector<char>> &matrix)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int max_area = 0;
+    int n = matrix.size();
+
+    int m = n ? matrix[0].size() : 0;
+
+    if (!n or !m)
+        return 0;
+
+    int ct = 0;
+    vector<int> arr(m, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == '1')
+            {
+                arr[j]++;
             }
+            else
+                arr[j] = 0;
         }
-        area = max(area, largestRectangleArea(arr[i]));
+        max_area = max(max_area, findLargestArea(arr));
     }
-    return area;
+
+    return max_area;
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     vector<int> arr(n);
-    
+
     return 0;
 }
