6549b03b3f003433c04b1e396c46f1f9c3368ccd
mittalprince <princemittal766@gmail.com>
2021-01-26 21:31:05 +0530

    MBinary Search Question

--- test.cpp
+++ test.cpp
@@ -1,111 +1,58 @@
-//https://leetcode.com/problems/create-sorted-array-through-instructions/
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
 typedef long long ll;
-#define ip(arr, n)              \
-	for (int i = 0; i < n; i++) \
-		cin >> arr[i];
-#define ip1(arr, n)              \
-	for (int i = 1; i <= n; i++) \
-		cin >> arr[i];
-#define op(arr, n)              \
-	for (int i = 0; i < n; i++) \
-		cout << arr[i] << " ";
-#define fstIO                         \
-	ios_base::sync_with_stdio(false); \
-	cin.tie(NULL);
-#define debug(x) cout << x << "\n";
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
 #define inf 1e9
 #define INF 1e16
-#define MOD 1000003
+#define MOD 1000000007
 
-vector<ll> BIT;
-vector<ll> arr;
-ll n;
+int solve(vector<int>&arr){
+	int n=arr.size();
+	if(n==1 || arr[0]!= arr[1])return arr[0];
 
-void init(ll i, ll val)
-{
-	ll index = i + 1;
-	while (index <= n)
-	{
-		BIT[index] = (BIT[index] + val) % MOD;
-		index += index & (-index);
-	}
-}
-
-void update(ll i, ll val)
-{
-	init(i, val - arr[i]);
-	arr[i] = val;
-}
-
-ll getSum(ll i)
-{
-	ll sum = 0;
-	ll index = i + 1;
-	while (index > 0)
-	{
-		sum = (sum + BIT[index]) % MOD;
-		index -= index & (-index);
-	}
-	return sum % MOD;
-}
-
-ll sumRange(ll left, ll right)
-{
-	return (getSum(right) - getSum(left - 1) + MOD) % MOD;
-}
-
-int main()
-{
-	cin >> n;
-	arr.resize(n);
-	ip(arr, n);
-
-	BIT = vector<ll>(n + 1, 0);
-	for (ll i = 0; i < n; i++)
-	{
-		ll t = 0;
-		if (arr[i] % 4 == 0)
-			t = arr[i];
-		else if (arr[i] % 4 == 1)
-			t = 1;
-		else if (arr[i] % 4 == 2)
-			t = arr[i] + 1;
-		arr[i] = t;
-	}
-
-	for (ll i = 0; i < n; i++)
-	{
-		init(i, arr[i]);
-		op(BIT, n);
-		cout << endl;
-	}
-
-	ll q;
-	cin >> q;
-	while (q--)
-	{
-		ll type, l, r;
-		cin >> type >> l >> r;
-		if (type == 1)
-		{
-			ll t = 0;
-			if (r % 4 == 0)
-				t = r;
-			else if (r % 4 == 1)
-				t = 1;
-			else if (r % 4 == 2)
-				t = r + 1;
-			update(l, t);
-			cout << "-1 ";
+	int s=0, e=n-1;
+	while(s<e){
+		int mid = (s+e)/2;
+		if(mid&1){
+			if(arr[mid]==arr[mid-1])s=mid+1;
+			else e=mid;
 		}
-		else
-		{
-			cout << sumRange(l, r) << " ";
+		else{
+			if(mid<n+1 and arr[mid]==arr[mid+1])s=mid+1;
+			else e=mid;
 		}
 	}
-	cout << endl;
+	return arr[s];
+}
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		vector<int> arr(n);
+		ip(arr, n);
+		cout<<solve(arr)<<endl;
+	}
 	return 0;
 }
+
+/*
+2
+11
+1 1 2 2 3 4 4 5 5 6 6
+11
+1 1 2 2 3 3 4 4 5 5 6
+*/
