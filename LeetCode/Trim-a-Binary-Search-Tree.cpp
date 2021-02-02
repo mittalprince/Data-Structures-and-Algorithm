8fb8ecb1596a01c15db6242cd17a99544f3b08f6
mittalprince <princemittal766@gmail.com>
2021-02-02 22:15:58 +0530

    Leetcode Daily Question

---
+++ LeetCode/Trim-a-Binary-Search-Tree.cpp
@@ -0,0 +1,51 @@
+//https://leetcode.com/problems/trim-a-binary-search-tree/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* trimBST(TreeNode* root, int L, int R) {
+    if(root==NULL)return NULL;
+    if(root->val<L){
+        return trimBST(root->right, L, R);
+    }
+    if(root->val>R){
+        return trimBST(root->left, L, R);
+    }
+
+    root->left=trimBST(root->left, L, R);
+    root->right=trimBST(root->right, L, R);
+
+    return root;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
