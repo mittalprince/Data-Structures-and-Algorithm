21c09b25888b62a42864e24fc5e48133770f02c5
mittalprince <princemittal766@gmail.com>
2021-02-12 22:21:15 +0530

    Leetcode Daily Question

---
+++ LeetCode/Number-of-Steps-to-Reduce-a-Number-to-Zero.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+int numberOfSteps (int n) {
+    int ans=0;
+    while(n>0){
+        if(n&1) n -= 1;
+        else n >>= 1;
+        ans++;
+    }
+    return ans;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
