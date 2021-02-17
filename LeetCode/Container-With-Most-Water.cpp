120b26b91bf66f5d4c7a413b43dd0492e1673eff
mittalprince <princemittal766@gmail.com>
2021-02-17 22:33:32 +0530

    Leetcode Daily Quuestion

---
+++ LeetCode/Container-With-Most-Water.cpp
@@ -0,0 +1,48 @@
+//https://leetcode.com/problems/container-with-most-water/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+int maxArea(vector<int>& height) {
+    int n=height.size();
+
+    int ans=0;
+    int l=0, r=n-1;
+    while(l<r){
+        ans = max(ans, min(height[l], height[r])*(r-l));
+        if(height[l]<=height[r]){
+            int hl=height[l];
+            l++;
+            while(l<r && height[l]<hl) l++;
+        }
+        else{
+            int hr=height[r];
+            r--;
+            while(l<r && height[r]<hr) r--;
+        }
+    }
+    return ans;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
