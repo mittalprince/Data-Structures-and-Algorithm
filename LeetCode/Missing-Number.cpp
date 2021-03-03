7db8f2a3159219386959a06d12abc7523e745159
mittalprince <princemittal766@gmail.com>
2021-03-03 22:33:45 +0530

    Leetcode Dailly Quuestion

---
+++ LeetCode/Missing-Number.cpp
@@ -0,0 +1,42 @@
+//
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+int missingNumber(vector<int>& nums) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n=nums.size();
+    int xor_val = n;
+
+    for(int i=0; i<n; i++){
+        xor_val ^= (i^nums[i]);
+    }
+
+    return xor_val;
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
