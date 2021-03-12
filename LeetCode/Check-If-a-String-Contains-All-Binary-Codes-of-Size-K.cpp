d4f80d4952a567be47c9fde05f12fa29532620f4
mittalprince <princemittal766@gmail.com>
2021-03-12 23:29:07 +0530

    Leetcode Daily Question

---
+++ LeetCode/Check-If-a-String-Contains-All-Binary-Codes-of-Size-K.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+bool hasAllCodes(string s, int k) {
+    if (k > s.size()) return false;
+    
+    unordered_set<string> my_set;
+    
+    for (int i = 0; i <= s.size()-k; i++)
+        my_set.insert(s.substr(i, k));
+    
+    return my_set.size() == pow(2, k);
+}
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
