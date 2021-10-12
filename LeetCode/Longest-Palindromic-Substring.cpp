6b9380d61360da7056f41a6b3af9f6e070dd5217
mittalprince <princemittal766@gmail.com>
2021-10-12 08:50:43 +0530

    Leetcode practice

--- LeetCode/Longest-Palindromic-Substring.cpp
+++ LeetCode/Longest-Palindromic-Substring.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 string longestPalindrome(string s){
@@ -45,6 +46,33 @@ string longestPalindrome(string s){
     }
     return s.substr(start, maxLength);
 } 
+
+int expandAroundCenter(string s, int left, int right){
+    if(s.length()==0 || left>right) return 0;
+
+    while(left>=0 && right<s.length() && s[left] == s[right]){
+        left--;
+        right++;
+    }
+    return right-left-1;
+}
+
+string longestPalindrome(string s){
+    int n=s.length();
+    if(n<=1) return s;
+    int start=0, end=0;
+    for(int i=0; i<n; i++){
+        int len1 = expandAroundCenter(s, i, i);
+        int len2 = expandAroundCenter(s, i, i+1);
+        int len = max(len1, len2);
+        if(len > end-start){
+            start = i-(len-1)/2;
+            end = i+len/2;
+        }
+    }
+    return s.substr(start, end-start+1);
+}
+ 
 int main(int agrc, char **argv){
     string s;
     cin>>s;
