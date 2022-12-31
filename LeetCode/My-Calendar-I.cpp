aa3e3868f3970c2dea7511a145677b52b83a317f
mittalprince <princemittal766@gmail.com>
2022-12-31 10:35:26 +0530

    Leeetcode practice

---
+++ LeetCode/My-Calendar-I.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/my-calendar-i/
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+vector<pair<int, int>> books;
+bool book(int start, int end) {
+    for (pair<int, int> p : books)
+        if (max(p.first, start) < min(end, p.second)) return false;
+    books.push_back({start, end});
+    return true;
+}
+
+int main(){
+
+/*
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+*/
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
