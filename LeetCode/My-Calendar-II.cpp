4ece19097f8227f150e6e57c014ba20b8405a3b3
mittalprince <princemittal766@gmail.com>
2022-12-31 10:35:46 +0530

    Leetcode practice

---
+++ LeetCode/My-Calendar-II.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/my-calendar-ii/
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+map<int, int>m;
+bool book(int start, int end) {
+    m[start]++;
+    m[end]--;
+    int ans=0, ongoing=0;
+    for(auto it: m){
+        ongoing += it.second;
+        ans = max(ans, ongoing);
+        if (ans>=3){
+            m[start]--;
+            m[end]++;
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+
+/*
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+*/
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
---
+++ LeetCode/My-Calendar-III.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/my-calendar-iii/
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+typedef long long ll;
+#define ip(arr, n) for(int i=0; i<n; i++) cin>>arr[i];
+#define ip1(arr, n) for(int i=1; i<=n; i++) cin>>arr[i];
+#define op(arr, n) for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+#define fstIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define debug(x) cout<<x<<"\n";
+#define inf 1e9
+#define INF 1e16
+#define MOD 1000000007
+
+map<int, int>m;
+
+int book(int start, int end) {
+    m[start]++;
+    m[end]--;
+    int ans=0, ongoing=0;
+    for(auto it: m){
+        ongoing += it.second;
+        ans = max(ans, ongoing);
+    }
+    return ans;
+}
+
+int main(){
+
+/*
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+*/
+
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    
+    return 0;
+}
