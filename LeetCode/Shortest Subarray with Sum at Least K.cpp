8bee4be71f0e5a2b8338899e8ea4971aed36fca2
mittalprince <princemittal766@gmail.com>
2024-11-17 17:40:49 +0530

    Sliding window practic

--- LeetCode/Shortest Subarray with Sum at Least K.cpp
+++ LeetCode/Shortest Subarray with Sum at Least K.cpp
@@ -14,16 +14,24 @@ typedef long long ll;
 #define MOD 1000000007
 
 int shortestSubarray(vector<int>& nums, int k) {
+    // https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/discuss/143726/C%2B%2BJavaPython-O(N)-Using-Deque
     int n = nums.size();
     vector<long long>temp(n, 0);
     temp[0] = nums[0];
     int ans = n+1;
     deque<int>d;
-
+    /*
+    variation of sliding window -> not exact as nums contains -ve values
+    in original one -> start-end -> condition break hoti hai toh start+1-end sabbhi ke liye valid hai
+    but in this case it's not aslwayys true as contain -ve so sum up and down ho sakta hai
+    */
     for(int i=0; i<n; i++){
         if(i) temp[i] = temp[i-1] + nums[i];
+        // storing prefix sum
         if(temp[i]>=k) ans = min(ans, i+1);
-
+        /*
+        in deque we store index which have monotonic increasing behavior prefix[d[i]] < prefix[d[i+1]]
+        */
         while(!d.empty() && temp[i] - temp[d.front()] >= k){
             ans = min(ans, i-d.front());
             d.pop_front();
